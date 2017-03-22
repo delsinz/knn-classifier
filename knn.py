26e24f1344ef4318182cec7872036902c8fda19b
delsin <coreinnovator@gmail.com>
2017-03-22 13:38:27 +1100

    Convert categorical to numeric

--- knn.py
+++ knn.py
@@ -3,11 +3,12 @@ import csv
 
 
 def main():
-    print(preprocess_data('data.data')[1])
+    print(preprocess_data('data.data')[0][0])
+    print(convert_categorical_attribute(preprocess_data('data.data')[0][0]))
 
 
 
-# Returns [[8 attributes], [class_label]]
+# Returns [[lists of 8 attributes], [class_label]]
 def preprocess_data(filename, abalone = 2):
     # Load data
     file = open(filename)
@@ -57,10 +58,19 @@ def compare_instance(instance_0, instance_1, method):
     return 0
 
 
-
-def handle_categorical_attribute(instance):
+# Break categorical attribute Sex into 3 binary attributes: M, F, I
+def convert_categorical_attribute(instance):
     # Assuming the attributes are provided in the given order
-    return 0
+    val = instance[0];
+    if val == 'M':
+        return [1, 0, 0] + instance[1:]
+    elif val == 'F':
+        return [0, 1, 0] + instance[1:]
+    elif val == 'I':
+        return [0, 0, 1] + instance[1:]
+    else:
+        return [0, 0, 0] + instance[1:]
+
 
 
 if __name__ == '__main__':
